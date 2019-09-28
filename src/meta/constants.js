8e53f355c7c285a42083475e8eddb2be88f9effb
gc <gc@users.noreply.github.com>
2019-09-29 03:01:06 +1000

    fix: these shouldnt be here...

--- src/meta/constants.js
+++ src/meta/constants.js
@@ -2,18 +2,6 @@ import { createBrowserHistory } from 'history';
 
 export const history = createBrowserHistory();
 
-export const BREAKPOINTS = {
-	xs: 0,
-	sm: 600,
-	md: 960,
-	lg: 1280,
-	xl: 1920
-};
-
-export const BREAKPOINT = Object.keys(BREAKPOINTS).reduce((obj, currentEle) => {
-	return { ...obj, [currentEle]: `@media (max-width: ${BREAKPOINTS[currentEle]}px)` };
-}, {});
-
 export const CLIENT_ID = '577488230539067403';
 export const BASE_WEB_URL = process.env.NODE_ENV === 'production' ? 'https://www.skyra.pw' : 'http://localhost:3000';
 export const BASE_API_URL = process.env.NODE_ENV === 'production' ? 'https://api.skyra.pw' : 'http://localhost:1234';
