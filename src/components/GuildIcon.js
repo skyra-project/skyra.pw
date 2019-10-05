20d706a01e897590e6d45de63b27e01d7e54f9f9
gc <gc@users.noreply.github.com>
2019-10-06 01:10:26 +1000

    src: add GuildIcon component

---
+++ src/components/GuildIcon.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Avatar } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+
+import { history } from 'meta/constants';
+import { getAcronym } from 'meta/util';
+
+const useStyles = makeStyles(theme => ({
+	defaultIcon: {
+		backgroundColor: theme.palette.secondary.main
+	}
+}));
+
+const GuildIcon = ({ guild }) => {
+	const classes = useStyles();
+	return (
+		<Avatar onClick={() => history.push(`/guilds/${guild.id}`)} src={guild.iconURL} className={classes.defaultIcon}>
+			{getAcronym(guild.name)}
+		</Avatar>
+	);
+};
+
+export default GuildIcon;
