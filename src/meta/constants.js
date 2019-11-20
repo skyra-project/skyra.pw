e8c815ee8c03e0b2768b7a435da65513f43759cc
Antonio Román <kyradiscord@gmail.com>
2019-11-20 13:00:47 +0100

    seo: Added icons and better PWA settings
    And far more

--- src/meta/constants.js
+++ src/meta/constants.js
@@ -2,7 +2,7 @@ import { createBrowserHistory } from 'history';
 
 export const history = createBrowserHistory();
 
-export const CLIENT_ID = process.env.NODE_ENV === 'production' ? '266624760782258186' : '577488230539067403';
+export const CLIENT_ID = process.env.NODE_ENV === 'production' ? '266624760782258186' : '365184854914236416';
 export const BASE_WEB_URL = process.env.NODE_ENV === 'production' ? 'https://skyra.netlify.com' : 'http://localhost:3000';
 export const BASE_API_URL = process.env.NODE_ENV === 'production' ? 'https://api.skyra.pw' : 'http://localhost:1234';
 export const BASE_CDN_URL = 'https://cdn.skyra.pw';
