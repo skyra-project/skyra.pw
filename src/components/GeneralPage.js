44dbb074f4c761dd57482fe61d82ae30d97fb4dd
gc <gc@users.noreply.github.com>
2019-11-27 06:55:50 +1100

    add user syncing

--- src/components/GeneralPage.js
+++ src/components/GeneralPage.js
@@ -1,4 +1,4 @@
-import React, { useGlobal } from 'reactn';
+import React, { Component } from 'reactn';
 import styled from 'styled-components';
 import { Box, Typography, Button, Container, LinearProgress } from '@material-ui/core';
 
@@ -6,6 +6,7 @@ import { oauthURL } from 'meta/constants';
 import UserMenu from 'components/UserMenu';
 import SkyraLogo from 'assets/skyraLogo';
 import Footer from 'components/Footer';
+import { syncUser } from 'meta/util';
 
 const PageContainer = styled.div`
 	display: flex;
@@ -14,31 +15,38 @@ const PageContainer = styled.div`
 	height: 100vh;
 `;
 
-export default ({ children, loading = false }) => {
-	const [global] = useGlobal();
-	const { authenticated } = global;
-	return (
-		<PageContainer>
-			<Container>
-				<Box p={1} my={3} display="flex" justifyContent="space-between" alignContent="center" alignItems="center">
-					<Box display="flex" justifyContent="space-around" alignContent="center" alignItems="center" minWidth={120}>
-						<SkyraLogo />
-						<Box display="flex" flexDirection="column" ml={3}>
-							<Typography variant="h5">Skyra</Typography>
-							<Typography variant="caption">The most advanced moderation bot.</Typography>
+class GeneralPage extends Component {
+	async componentDidMount() {
+		syncUser();
+	}
+	render() {
+		const { children, loading = false } = this.props;
+		const { authenticated } = this.global;
+		return (
+			<PageContainer>
+				<Container>
+					<Box p={1} my={3} display="flex" justifyContent="space-between" alignContent="center" alignItems="center">
+						<Box display="flex" justifyContent="space-around" alignContent="center" alignItems="center" minWidth={120}>
+							<SkyraLogo />
+							<Box display="flex" flexDirection="column" ml={3}>
+								<Typography variant="h5">Skyra</Typography>
+								<Typography variant="caption">The most advanced moderation bot.</Typography>
+							</Box>
 						</Box>
-					</Box>
-					{authenticated && <UserMenu />}
+						{authenticated && <UserMenu />}
 
-					{!authenticated && !loading && (
-						<Button href={oauthURL} variant="contained" color="secondary">
-							Log In
-						</Button>
-					)}
-				</Box>
-			</Container>
-			{loading ? <LinearProgress variant="query" /> : children}
-			<Footer />
-		</PageContainer>
-	);
-};
+						{!authenticated && !loading && (
+							<Button href={oauthURL} variant="contained" color="secondary">
+								Log In
+							</Button>
+						)}
+					</Box>
+				</Container>
+				{loading ? <LinearProgress variant="query" /> : children}
+				<Footer />
+			</PageContainer>
+		);
+	}
+}
+
+export default GeneralPage;
