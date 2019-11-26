156d4730ed458bfe28e00121e35762e300bf215f
gc <gc@users.noreply.github.com>
2019-11-27 06:57:07 +1100

    fix loading bug in guild callback page

--- src/pages/oauth/GuildCallbackPage.js
+++ src/pages/oauth/GuildCallbackPage.js
@@ -8,7 +8,7 @@ class GuildCallbackPage extends Component {
 		guildID: null
 	};
 
-	componentWillMount() {
+	componentDidMount() {
 		const urlParams = new URLSearchParams(window.location.search);
 		const id = urlParams.get('guild_id');
 
@@ -16,7 +16,11 @@ class GuildCallbackPage extends Component {
 	}
 
 	render() {
-		return <GeneralPage loading>{this.state.guildID && <Redirect to={`/guilds/${this.state.guildID}`} />}</GeneralPage>;
+		return (
+			<GeneralPage loading={!this.state.guildID}>
+				{this.state.guildID && <Redirect to={`/guilds/${this.state.guildID}`} />}
+			</GeneralPage>
+		);
 	}
 }
 
