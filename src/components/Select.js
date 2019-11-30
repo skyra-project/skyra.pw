f9984a19d694e4a9c03da1fca8d5841db27db6de
gc <gc@users.noreply.github.com>
2019-11-30 19:52:03 +1100

    add error prop to Select

--- src/components/Select.js
+++ src/components/Select.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FormControl, InputLabel, Select as MSelect } from '@material-ui/core';
 
-const Select = ({ title, onChange, value, children, variant = 'outlined' }) => {
+const Select = ({ title, onChange, value, children, error, variant = 'outlined' }) => {
 	const inputLabel = React.useRef(null);
 	const [labelWidth, setLabelWidth] = React.useState(0);
 	React.useEffect(() => {
@@ -9,7 +9,7 @@ const Select = ({ title, onChange, value, children, variant = 'outlined' }) => {
 	}, []);
 
 	return (
-		<FormControl variant={variant}>
+		<FormControl error={error} variant={variant}>
 			<InputLabel ref={inputLabel}>{title}</InputLabel>
 			<MSelect native value={value} onChange={onChange} labelWidth={labelWidth}>
 				{children}
