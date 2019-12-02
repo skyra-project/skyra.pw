42ed858379823cfc87cd783ae767db3fc0f4559e
gc <gc@users.noreply.github.com>
2019-12-02 22:09:17 +1100

    fix scrollbar color/width

--- src/components/CssBaseline.js
+++ src/components/CssBaseline.js
@@ -4,10 +4,10 @@ import { withStyles } from '@material-ui/core/styles';
 const styles = () => ({
 	'@global': {
 		'*::-webkit-scrollbar': {
-			width: '0.3em'
+			width: '0.45em'
 		},
 		'*::-webkit-scrollbar-track': {
-			'backgroundColor': 'rgba(113, 137, 218, 0.0.5)',
+			backgroundColor: 'rgba(0, 0, 0, 0.35)',
 			'-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
 		},
 		'*::-webkit-scrollbar-thumb': {
