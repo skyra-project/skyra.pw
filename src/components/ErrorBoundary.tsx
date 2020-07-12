fe67471c10b1000e91bec80df183c028f520c38e
Jeroen Claassens <support@favware.tech>
2020-07-12 12:22:26 +0200

    fix: fixed error boundary page

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,14 +1,37 @@
+import { ButtonGroup, createStyles, withStyles, WithStyles } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import ForumIcon from '@material-ui/icons/Forum';
+import HomeIcon from '@material-ui/icons/Home';
+import { history } from 'lib/util/constants';
 import React, { Component } from 'react';
-import NotFound from 'pages/NotFound';
 
-type ErrorBoundaryProps = Record<PropertyKey, unknown>;
+const styles = createStyles({
+	root: {
+		overflowY: 'hidden'
+	},
+	text: {
+		lineHeight: 1.6,
+		textAlign: 'center',
+		marginBottom: 40
+	}
+});
+
+function navigate(path: string) {
+	if (path.startsWith('http')) {
+		return () => (window.location.href = path);
+	}
+	return () => history.push(path);
+}
+
+type ErrorBoundaryProps = WithStyles<typeof styles>;
 type ErrorBoundaryState = Record<PropertyKey, unknown> & { hasError: boolean };
 
-export default class extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-	constructor(props: ErrorBoundaryProps) {
-		super(props);
-		this.state = { hasError: false };
-	}
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+	state = { hasError: false };
 
 	static getDerivedStateFromError(error: any) {
 		console.log(error);
@@ -16,17 +39,50 @@ export default class extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 		return { hasError: true };
 	}
 
-	// componentDidCatch(error, errorInfo) {
-	// 	// You can also log the error to an error reporting service
-	// 	logErrorToMyService(error, errorInfo);
-	// }
+	componentDidCatch() {
+		this.setState({ hasError: true });
+	}
 
 	render() {
+		const { classes, children } = this.props;
+
 		if (this.state.hasError) {
 			// You can render any custom fallback UI
-			return <NotFound />;
+			return (
+				<Container maxWidth="md">
+					<Grid
+						container
+						direction="column"
+						justify="center"
+						alignContent="stretch"
+						alignItems="center"
+						classes={{ root: classes.root }}
+					>
+						<Grid item>
+							<Typography variant="h4" color="textPrimary" classes={{ root: classes.text }}>
+								Woaw! You just ran into a non-existing page! If you think you got here by mistake then feel free to join the
+								support server on Discord using the button below and let us know.
+							</Typography>
+						</Grid>
+						<Grid item>
+							<Box>
+								<ButtonGroup variant="contained" color="primary" size="small">
+									<Button onClick={navigate('https://join.skyra.pw')} startIcon={<ForumIcon />}>
+										Join Support Server
+									</Button>
+									<Button onClick={navigate('/')} startIcon={<HomeIcon />}>
+										Go Back Home
+									</Button>
+								</ButtonGroup>
+							</Box>
+						</Grid>
+					</Grid>
+				</Container>
+			);
 		}
 
-		return this.props.children;
+		return children;
 	}
 }
+
+export default withStyles(styles)(ErrorBoundary);
