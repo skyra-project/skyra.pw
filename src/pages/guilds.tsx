1a017dc71ccc13bb9dfb073b5b02eebdd2b9631b
Jeroen Claassens <support@favware.tech>
2020-10-22 22:54:27 +0200

    fix(guilds): fix mobile layout

--- src/pages/guilds.tsx
+++ src/pages/guilds.tsx
@@ -1,7 +1,7 @@
 import { createSeoProps } from '@config/next-seo.config';
 import { useAuthenticated } from '@contexts/AuthenticationContext';
 import { useDiscordPack } from '@contexts/DiscordPackContext';
-import { Box, Container, createStyles, makeStyles } from '@material-ui/core';
+import { Box, Container, createStyles, makeStyles, Theme } from '@material-ui/core';
 import FilteredGuildCards from '@presentational/GuildCard';
 import GeneralPage from '@presentational/Layout/General';
 import ScrollToTop from '@routing/ScrollToTop';
@@ -9,15 +9,17 @@ import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
 import React from 'react';
 
-const useStyles = makeStyles(() =>
+const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		container: {
 			display: 'flex',
 			flexDirection: 'column',
 			justifyContent: 'center',
-			height: 'calc(100vh - 200px - 128px)',
 			alignContent: 'center',
-			alignItems: 'center'
+			alignItems: 'center',
+			[theme.breakpoints.up('md')]: {
+				height: 'calc(100vh - 200px - 128px)'
+			}
 		}
 	})
 );
