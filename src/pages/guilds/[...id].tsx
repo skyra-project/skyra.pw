fefc727c802e2815173ef34ee130cbbcbf3d7d2e
Jeroen Claassens <support@favware.tech>
2020-10-24 21:06:52 +0200

    feat: setup guild settings pages

--- src/pages/guilds/[...id].tsx
+++ src/pages/guilds/[...id].tsx
@@ -1,16 +1,32 @@
 import { createSeoProps } from '@config/next-seo.config';
-import { Typography } from '@material-ui/core';
+import { useAuthenticated } from '@contexts/AuthenticationContext';
+import Dashboard from '@layout/Settings/Dashboard';
+import Typography from '@material-ui/core/Typography';
+import RedirectRoute from '@routing/RedirectRoute';
 import { NextPage } from 'next';
 import { NextSeo } from 'next-seo';
+import { useRouter } from 'next/router';
 import React from 'react';
 
-const CustomCommandsSettings: NextPage = () => {
+const GuildSettingsPage: NextPage = () => {
+	const router = useRouter();
+	const authenticated = useAuthenticated();
+
+	if (!authenticated) {
+		return <RedirectRoute redirectUri="/" />;
+	}
+
+	const [guildId, ...path] = router.query.id;
+	console.log(path);
+
 	return (
 		<>
 			<NextSeo {...createSeoProps({ title: 'Homepage' })} />
-			<Typography>Guild Settings Base page!</Typography>
+			<Dashboard guildId={guildId}>
+				<Typography>Guild Settings Base page!</Typography>
+			</Dashboard>
 		</>
 	);
 };
 
-export default CustomCommandsSettings;
+export default GuildSettingsPage;
