1a92601d5951c78e047d28a73e65da51b8ef495e
Jeroen Claassens <support@favware.tech>
2020-10-24 19:11:25 +0200

    feat: handle not authenticated for guilds page

--- src/pages/guilds.tsx
+++ src/pages/guilds.tsx
@@ -1,7 +1,11 @@
 import { createSeoProps } from '@config/next-seo.config';
 import { useAuthenticated } from '@contexts/AuthenticationContext';
 import { useDiscordPack } from '@contexts/DiscordPackContext';
-import { Box, Container, createStyles, makeStyles, Theme } from '@material-ui/core';
+import { useMobileContext } from '@contexts/MobileContext';
+import { createStyles, makeStyles, Theme } from '@material-ui/core';
+import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
 import FilteredGuildCards from '@presentational/GuildCard';
 import GeneralPage from '@presentational/Layout/General';
 import ScrollToTop from '@routing/ScrollToTop';
@@ -17,30 +21,46 @@ const useStyles = makeStyles((theme: Theme) =>
 			justifyContent: 'center',
 			alignContent: 'center',
 			alignItems: 'center',
+			height: ({ authenticated }: { authenticated: boolean }) => (authenticated ? 'unset' : 'calc(100vh - 200px - 128px)'),
 			[theme.breakpoints.up('md')]: {
 				height: 'calc(100vh - 200px - 128px)'
+			}
+		},
+		notSignedInText: {
+			fontSize: theme.typography.pxToRem(40),
+			[theme.breakpoints.down('sm')]: {
+				fontSize: theme.typography.pxToRem(30)
 			}
 		}
 	})
 );
 
 const GuildsPage: NextPage = () => {
-	const classes = useStyles();
 	const authenticated = useAuthenticated();
+	const classes = useStyles({ authenticated });
 	const pack = useDiscordPack();
+	const { isMobile } = useMobileContext();
 
 	return (
 		<>
 			<NextSeo {...createSeoProps({ title: 'Guilds' })} />
 			<ScrollToTop />
 			<GeneralPage>
-				{authenticated && (
-					<Container classes={{ root: classes.container }}>
-						<Box display="flex" flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center">
-							{FilteredGuildCards(pack)}
-						</Box>
-					</Container>
-				)}
+				<Container classes={{ root: classes.container }}>
+					<Box display="flex" flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center">
+						{authenticated ? (
+							FilteredGuildCards(pack)
+						) : (
+							<Typography variant="h1" classes={{ root: classes.notSignedInText }}>
+								Sorry, you are not logged in and can therefore not view this page. Please{' '}
+								{isMobile
+									? 'open the menu button at the top left then click "Login"'
+									: 'click the "Login" button at the top right'}{' '}
+								to login with Discord
+							</Typography>
+						)}
+					</Box>
+				</Container>
 			</GeneralPage>
 		</>
 	);
