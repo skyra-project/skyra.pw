56b6675e3748eff6c8342ab45e1a2a5e2162a3c6
Jeroen Claassens <support@favware.tech>
2020-10-28 23:43:02 +0100

    fix: remove i18n from nextjs buildin, it doesn't work yet

--- src/next.config.js
+++ src/next.config.js
@@ -5,9 +5,5 @@ module.exports = withPWA({
 	pwa: {
 		dest: 'public',
 		disable: process.env.NODE_ENV === 'development'
-	},
-	i18n: {
-		locales: ['en-US', 'es-ES', 'nl-NL'],
-		defaultLocale: 'en-US'
 	}
 });
