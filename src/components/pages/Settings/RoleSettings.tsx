f9f58eb607747da88b16738afbfcd9b35229ce7a
Jeroen Claassens <support@favware.tech>
2020-11-29 21:38:50 +0100

    chore: add type for common props

--- src/components/pages/Settings/RoleSettings.tsx
+++ src/components/pages/Settings/RoleSettings.tsx
@@ -6,8 +6,8 @@ import PageHeader from '#layout/Settings/PageHeader';
 import Section from '#layout/Settings/Section';
 import SimpleGrid from '#mui/SimpleGrid';
 import SelectBoolean from '#selects/SelectBoolean';
-import SelectRole from '#selects/SelectRole';
-import SelectRoles from '#selects/SelectRoles';
+import SelectRole, { SelectRoleProps } from '#selects/SelectRole';
+import SelectRoles, { SelectRolesProps } from '#selects/SelectRoles';
 import { cast } from '#utils/util';
 import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
@@ -31,6 +31,8 @@ const useStyles = makeStyles((theme: Theme) =>
 		}
 	})
 );
+
+type SelectCommonProps = Omit<SelectRoleProps, 'value' | 'onChange'> & Omit<SelectRolesProps, 'value' | 'onChange'> & { key: number };
 
 const RoleSettings: FC = () => {
 	const classes = useStyles();
@@ -78,7 +80,7 @@ const RoleSettings: FC = () => {
 					}}
 				>
 					{ConfigurableRoles.map(({ name, tooltip, key }, index) => {
-						const props = {
+						const props: SelectCommonProps = {
 							key: index,
 							label: name,
 							guild: guildData,
