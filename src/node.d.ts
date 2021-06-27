2b7c3ec1b65d787f3482bce44a8c89740528f37c
Jeroen Claassens <support@favware.tech>
2021-06-27 16:15:45 +0200

    chore: bump dependencies
    closes #290

---
+++ src/node.d.ts
@@ -0,0 +1,9 @@
+declare namespace NodeJS {
+	interface ProcessEnv {
+		readonly NODE_ENV: 'development' | 'production' | 'test';
+		readonly NEXT_PUBLIC_CLIENT_ID: string;
+		readonly NEXT_PUBLIC_BASE_WEB_URL: string;
+		readonly NEXT_PUBLIC_BASE_API_URL: string;
+		readonly NEXT_PUBLIC_WS_URL: string;
+	}
+}
