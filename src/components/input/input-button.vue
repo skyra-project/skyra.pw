21e2d7f89136abd635a91053d222a7cbfef654e5
Antonio Román <kyradiscord@gmail.com>
2021-09-01 19:30:33 +0200

    chore: and if you ask me how i'm feeling

---
+++ src/components/input/input-button.vue
@@ -0,0 +1,26 @@
+<template>
+	<button type="button" class="rounded-md p-2 focus:ring ring-offset-2" :class="getColors(level)">
+		<slot></slot>
+	</button>
+</template>
+
+<script setup lang="ts">
+defineProps<{ level?: Level }>();
+
+function getColors(level: Level = 'none') {
+	switch (level) {
+		case 'none':
+			return 'hover:bg-white hover:bg-opacity-10 focus:bg-white focus:bg-opacity-10';
+		case 'success':
+			return 'bg-green-500 hover:bg-green-600 ring-green-600';
+		case 'warning':
+			return 'bg-yellow-500 hover:bg-yellow-600 ring-yellow-600';
+		case 'error':
+			return 'bg-red-500 hover:bg-red-600 ring-red-600';
+		case 'info':
+			return 'bg-blue-500 hover:bg-blue-600 ring-blue-600';
+	}
+}
+
+type Level = 'success' | 'warning' | 'error' | 'info' | 'none';
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,18 +1,23 @@
 <template>
 	<h1>Hello!</h1>
+	<input-button>Aaa</input-button>
+	<input-button level="info">Aaa</input-button>
+	<input-button level="success">Aaa</input-button>
+	<input-button level="warning">Aaa</input-button>
+	<input-button level="error">Aaa</input-button>
+
+	<br />
+
 	<input-switch v-model:value="checked" />
 	{{ checked ? 'Checked!' : 'Unchecked!' }}
-
 	<br />
 
 	<input-text v-model:value="text" />
 	{{ text }}
-
 	<br />
 
 	<input-slider v-model:value="slider" :min="0" :max="100" />
 	{{ slider }}
-
 	<br />
 
 	<input-select
@@ -24,10 +29,22 @@
 		]"
 	/>
 	{{ city }}
+	<br />
+	<input-notification>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="info" light>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="success">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="success" light>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="warning">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="warning" light>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="error">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
+	<input-notification level="error" light>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</input-notification>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import InputButton from '~/components/input/input-button.vue';
+import InputNotification from '~/components/input/input-notification.vue';
 import InputSwitch from '~/components/input/input-switch.vue';
 import InputText from '~/components/input/input-text.vue';
 import InputSlider from '~/components/input/input-slider.vue';
