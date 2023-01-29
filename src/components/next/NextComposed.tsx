72ae3f671b46cb8ece94729be654bf087766309e
Jeroen Claassens <support@favware.tech>
2023-01-29 15:32:36 +0100

    fix(nextlink): ensure a is not nested under a

--- src/components/next/NextComposed.tsx
+++ src/components/next/NextComposed.tsx
@@ -1,9 +1,9 @@
-import { styled, Typography } from '@mui/material';
+import { Box, styled, Typography } from '@mui/material';
 import NextLink, { LinkProps as NextLinkProps } from 'next/link';
 import { forwardRef, type AnchorHTMLAttributes, type PropsWithChildren } from 'react';
 
 // Add support for the sx prop for consistency with the other branches.
-const Anchor = styled('a')({});
+const Anchor = styled(Box)({});
 
 export interface NextLinkComposedProps
 	extends Omit<AnchorHTMLAttributes<HTMLAnchorElement>, 'href' | `on${string}`>,
@@ -14,7 +14,19 @@ export interface NextLinkComposedProps
 
 const NextLinkComposed = forwardRef<HTMLAnchorElement, PropsWithChildren<NextLinkComposedProps>>(
 	({ to, linkAs, replace, scroll, shallow, prefetch, locale, ...other }, ref) => (
-		<NextLink href={to} prefetch={prefetch} as={linkAs} replace={replace} scroll={scroll} shallow={shallow} passHref locale={locale}>
+		<NextLink
+			href={to}
+			prefetch={prefetch}
+			as={linkAs}
+			replace={replace}
+			scroll={scroll}
+			shallow={shallow}
+			passHref
+			locale={locale}
+			style={{
+				textDecoration: 'none'
+			}}
+		>
 			<Typography component="span" variant="body2" color="textPrimary">
 				<Anchor ref={ref} {...other} />
 			</Typography>
