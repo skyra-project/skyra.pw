904a600122c2a9cdb06bc93ce6210a6ff202b57f
Jeroen Claassens <support@favware.tech>
2023-02-01 00:31:20 +0100

    chore: locating the required gigapixels to render...

--- src/components/presentational/Layout/Settings/Navigation/SettingsDrawerItems.tsx
+++ src/components/presentational/Layout/Settings/Navigation/SettingsDrawerItems.tsx
@@ -6,7 +6,6 @@ import EventIcon from '@mui/icons-material/EventNote';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
 import CustomCommandsIcon from '@mui/icons-material/Extension';
-import FeedbackIcon from '@mui/icons-material/Feedback';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import ChannelsIcon from '@mui/icons-material/Forum';
 import GavelIcon from '@mui/icons-material/Gavel';
