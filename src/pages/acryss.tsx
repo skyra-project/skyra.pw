d3f9592b166a9bc65485519a4b954222c1351693
Aura Román <kyradiscord@gmail.com>
2024-03-31 23:08:59 +0200

    feat: add `/acryss`

---
+++ src/pages/acryss.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+
+const Acryss = () => {
+	useEffect(() => {
+		window.location.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+	}, []);
+
+	return null;
+};
+
+export default Acryss;
